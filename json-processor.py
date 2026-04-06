20c5c1904cd24aba14ca048e1245324e3c12b9e0
Mark Schaver <schaver@gmail.com>
2026-04-06 18:11:57 -0400

    Yahoo fix.

--- json-processor.py
+++ json-processor.py
@@ -105,6 +105,8 @@ def process_search_results(results_json):
                 item_source = results_json["items"][i]["displayLink"]
                 if item_source == 'apnews.com':
                     item_source = re.sub('apnews.com', 'www.apnews.com', item_source)
+                if 'yahoo.com' in item_source:
+                    item_source = 'www.yahoo.com'
                 item_phrase = results_json["queries"]["request"][0]["searchTerms"]
                 item_title = results_json["items"][i]["title"]
                 item_link = results_json["items"][i]["link"]
